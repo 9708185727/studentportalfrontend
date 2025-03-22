0d805f30df55327435d3dc55aa27dd33135f0319
raman <ramancdry2058@gmail.com>
2025-03-22 09:26:59 +0545

    'added'

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import adobe from '../../assets/imageadobe.png'
+
 import { BUY_ROUTE } from '../../constants/route'
 import { Link } from 'react-router-dom'
+import nodejs from '../../assets/coursesimages/nodejs.png'
 const course={
   title:"Adobe Course",
   description:"this is Adobe courses",
@@ -13,7 +14,7 @@ const DashboardCard = () => {
 
   return (
     <div className=" text-black pb-8 rounded-2xl shadow-lg w-full  ">
-      <img src={adobe} alt="nodejs" className="rounded-lg w-full h-1/2"/>
+      <img src={nodejs} alt="nodejs" className="rounded-lg w-full h-1/2"/>
      <div className='ml-4 mt-2'>
      <h2 className="text-xl font-bold mb-2">{course.title}</h2>
       <p className="text-sm mb-3">{course.description}</p>
